f63621e03c904842622fd02bd3c5c11ccdf1bd30
Ftarquip <ftarquip@unjbg.edu.pe>
2025-06-16 16:46:04 -0500

    Desafio 2 subiendo

--- Desafio_2/main.py
+++ Desafio_2/main.py
@@ -1,36 +1,33 @@
-#!/usr/bin/env pybricks-micropython
+# pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Color
+from pybricks.tools import wait
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-# Create your objects here.
+# Inicializar ladrillo y dispositivos
 ev3 = EV3Brick()
-color_sensor = ColorSensor(Port.S3) 
+motorA = Motor(Port.B)
+motorB = Motor(Port.C)
+sensor_color = ColorSensor(Port.S3)
 
-# Write your program here.
-ev3.speaker.beep()
 
-mA = Motor(Port.B)
-mB = Motor(Port.C)
+motorA.run(300)
+motorB.run(300)
 
-tiempo_recorrido
-color_detectado = color_sensor.color()
-color_objetivo = Color.RED
 
-mA.run(-300)
-mB.run(-300)
+while 1==1:
+    if sensor_color.color() == Color.RED:
 
-while color_detectado=color_objetivo:
-    if color_detectado == color_objetivo:    
-        break()
-        color = color_sensor.color()
-    sleep(0.1)
-mA.run(tiempo_recorrido)
-mB.run(tiempo_recorrido)
+        motorA.stop()
+        motorB.stop()
+
+        ev3.speaker.beep()
+
+        wait(1000)
+
+        motorA.run_time(-300, 1000)
+        motorB.run_time(-300, 1000)
+
+        break  
+
+    wait(100)
